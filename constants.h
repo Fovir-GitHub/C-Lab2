689126e5076299d4a04e24df2543b496bb70c759
Fovir <fovir@disroot.org>
2024-11-26 15:07:44 +0800

    add comment to LINK_LIST_STATUS_CODES

--- constants.h
+++ constants.h
@@ -1,10 +1,13 @@
 #ifndef _CONSTANTS_H_
 #define _CONSTANTS_H_
 
+/**
+ *@brief the status code of link list's operation
+ */
 enum LINK_LIST_STATUS_CODES {
-	SUCCESS_ADD,
-	FAILED_ALLOCATE_MEMORY,
-	DUPLICATED_NODE
+	SUCCESS_ADD,            /* add new node successfully */
+	FAILED_ALLOCATE_MEMORY, /* can't allocate memory for new node */
+	DUPLICATED_NODE         /* the node item (category) exists */
 };
 
 #endif // !_CONSTANTS_H_
