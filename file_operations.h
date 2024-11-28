74cc044d58d27d4b387b7f1c7d7f48e904fb44f8
Fovir <fovir@disroot.org>
2024-11-28 19:55:58 +0800

    add file operation header

---
+++ file_operations.h
@@ -0,0 +1,10 @@
+#ifndef _FILE_OPERATIONS_H_
+#define _FILE_OPERATIONS_H_
+
+#include "category.h"
+#include "item.h"
+#include "my_utility.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif // !_FILE_OPERATIONS_H_
