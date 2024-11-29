556370b9317636920918e18956d03b0ed17c288f
Fovir <fovir@disroot.org>
2024-11-29 12:04:18 +0800

    test addItemstoCategory() function

--- main.c
+++ main.c
@@ -6,14 +6,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void show(LinkListNode * node)
+{
+    puts(node->category_item.category_name);
+    showAVLTree(&node->category_item.item_tree);
+
+    return;
+}
+
 int main(void)
 {
-    LinkList category_link_list;
+    const int N = 3;
 
-    initializeLinkList(&category_link_list);
-    readCategoryDatafromFile(&category_link_list);
+    LinkList category_list;
+    char * category_name;
+    char * item_name;
+    char * remove_category_name;
 
-    showCategoryLinkList(&category_link_list);
+    initializeLinkList(&category_list);
 
+    puts("Enter 3 category name:");
+    for (int i = 0; i < N; i++)
+        addCategorytoLinkList(
+            &category_list,
+            getString(&category_name, CATEGORY_NAME_MAX_LENGTH));
+
+    puts("Enter 3 category and item name:");
+    for (int i = 0; i < N; i++)
+    {
+        printf("Please enter category's name: ");
+        getString(&category_name, CATEGORY_NAME_MAX_LENGTH);
+        printf("Please enter item's name: ");
+        getString(&item_name, ITEM_NAME_MAX_LENGTH);
+
+        addItemstoCategory(&category_list, makeItem(category_name, item_name),
+                           CREATE_NEW_CATEGORY);
+    }
+
+    traverseCategoryLinkList(&category_list, show);
+
+    freeCategoryLinkList(&category_list);
     return 0;
 }
