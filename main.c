77763d74e929e3979a88e12aeb71575e86a1fe68
Fovir <fovir@disroot.org>
2024-11-29 13:32:23 +0800

    test read from file functions test traverseCategoryLinkList function

--- main.c
+++ main.c
@@ -16,35 +16,11 @@ void show(LinkListNode * node)
 
 int main(void)
 {
-    const int N = 3;
-
     LinkList category_list;
-    char * category_name;
-    char * item_name;
-    char * remove_category_name;
 
     initializeLinkList(&category_list);
-
-    puts("Enter 3 category name:");
-    for (int i = 0; i < N; i++)
-        addCategorytoLinkList(
-            &category_list,
-            getString(&category_name, CATEGORY_NAME_MAX_LENGTH));
-
-    puts("Enter 3 category and item name:");
-    for (int i = 0; i < N; i++)
-    {
-        printf("Please enter category's name: ");
-        getString(&category_name, CATEGORY_NAME_MAX_LENGTH);
-        printf("Please enter item's name: ");
-        getString(&item_name, ITEM_NAME_MAX_LENGTH);
-
-        addItemstoCategory(&category_list, makeItem(category_name, item_name),
-                           CREATE_NEW_CATEGORY);
-    }
+    readCategoryDatafromFile(&category_list);
+    readItemsDatafromFile(&category_list);
 
     traverseCategoryLinkList(&category_list, show);
-
-    freeCategoryLinkList(&category_list);
-    return 0;
 }
