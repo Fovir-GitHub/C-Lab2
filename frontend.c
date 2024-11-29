821e770a656c8d3e5f65a23befdae91a9dee5ee1
Fovir <fovir@disroot.org>
2024-11-29 19:14:54 +0800

    add printMenuFrame() function and centerMenuTitle() funciton

--- frontend.c
+++ frontend.c
@@ -2,6 +2,18 @@
 #include <stdio.h>
 #include <string.h>
 
+int centerMenuTitle(char * title)
+{
+    return (MENU_WIDTH - strlen(title)) / 2;
+}
+
+void printMenuFrame(char frame_character)
+{
+    for (int i = 0; i < MENU_WIDTH; i++)
+        putchar(frame_character);
+    putchar('\n');
+}
+
 void showMainMenu()
 {
     char * title = "Main Menu";
--- frontend.h
+++ frontend.h
@@ -4,6 +4,21 @@
 #define MENU_WIDTH 40 /* the width of menu */
 
 /**
+ *@brief Center the title.
+ *
+ * @param title the menu title
+ * @return int the prefix space length of the center title
+ */
+int centerMenuTitle(char * title);
+
+/**
+ *@brief Print the menu frame with the character.
+ *
+ * @param frame_character the frame character
+ */
+void printMenuFrame(char frame_character);
+
+/**
  * Main menu include:
  * 1. show all categories
  * 2. add category
