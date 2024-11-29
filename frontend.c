fd2d6bfe5f75457272adbf67c981394b13b836fb
Fovir <fovir@disroot.org>
2024-11-29 19:03:09 +0800

    add enum for main menu

---
+++ frontend.c
@@ -0,0 +1,8 @@
+#include "frontend.h"
+#include <stdio.h>
+#include <string.h>
+
+void showMainMenu()
+{
+    char * title = "Main Menu";
+}
--- frontend.h
+++ frontend.h
@@ -1,6 +1,31 @@
 #ifndef _FRONTEND_H_
 #define _FRONTEND_H_
 
+/**
+ * Main menu include:
+ * 1. show all categories
+ * 2. add category
+ * 3. remove category
+ * 4. show all items
+ * 5. show items according categories
+ * 6. add item
+ * 7. remove item
+ * 8. edit item information
+ * q. quit
+ */
 
+enum MAIN_MENU_OPTIONS {
+    SHOW_ALL_CATEGORIES,           /* show all categories */
+    ADD_CATEGORY,                  /* add category */
+    REMOVE_CATEGORY,               /* remove category */
+    SHOW_ALL_ITEMS,                /* show all items */
+    SHOW_ITEMS_ACCORDING_CATEGORY, /* show items according categories */
+    ADD_ITEM,                      /* add item */
+    REMOVE_ITEM,                   /* remove item */
+    EDIT_ITEM,                     /* edit item information */
+    QUIT                           /* quit */
+};
+
+void showMainMenu();
 
 #endif // !_FRONTEND_H_
