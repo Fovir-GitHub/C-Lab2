2a2f6eba80def30a1f1fdca4b4caf62fd74a27ed
Fovir <fovir@disroot.org>
2024-12-02 19:46:50 +0800

    clear test

--- main.c
+++ main.c
@@ -9,16 +9,4 @@
 
 int main(void)
 {
-    char user_choice = 0;
-
-    showMainMenu();
-    while (!(user_choice = getChoicefromUser("12345678q")))
-    {
-        printf("You entered an invalid choice! Press any key to continue...");
-        getchar();
-        clearScreen();
-        showMainMenu();
-    }
-    printf("choice: %c\n", user_choice);
-    return 0;
 }
