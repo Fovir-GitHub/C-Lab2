2497bc6636382af2aa2665267ef3b7d9cb2ac50d
Fovir <fovir@disroot.org>
2024-12-03 21:29:29 +0800

    test link list

--- main.c
+++ main.c
@@ -9,10 +9,23 @@
 
 int main(void)
 {
+    const int N = 3;
     LinkList categories;
+    char * remove_name = NULL;
 
     initializeLinkList(&categories);
-    readCategoryDatafromFile(&categories);
-    showCategories(&categories);
+
+    for (int i = 0; i < N; i++)
+    {
+        getString(&remove_name, CATEGORY_NAME_MAX_LENGTH);
+        addCategorytoLinkList(&categories, remove_name);
+    }
+
+    while (getString(&remove_name, CATEGORY_NAME_MAX_LENGTH))
+    {
+        removeCategoryfromLinkList(&categories, remove_name);
+        putchar('\n');
+        showCategoryLinkList(&categories);
+    }
     return 0;
 }
