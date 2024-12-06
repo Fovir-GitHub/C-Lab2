6de8a7566d10fb74400e6ee3f40e7164e0e047fe
Fovir <fovir@disroot.org>
2024-12-06 20:22:48 +0800

    update addItem function

--- frontend.c
+++ frontend.c
@@ -477,7 +477,7 @@ void addItem(LinkList * list)
         printf("Failed to add the item! ");
     else
     {
-        printf("Add item successfully! ");
+        puts("Add item successfully!");
         showItemInformation(&temp_item);
     }
 
