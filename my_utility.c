5d7011058e6774b8cd6c4f9ec6b24ccc5173d506
Fovir <fovir@disroot.org>
2024-12-09 15:08:52 +0800

    update legalString() check for empty pointer

--- my_utility.c
+++ my_utility.c
@@ -92,6 +92,9 @@ bool legalString(char * str)
     static const char * ILLEGAL_CHRACTERS = "@";
     char * iter = str;
 
+    if (!str)
+        return false;
+
     if (strcmp(str, "quit") == 0)
         return false;
 
